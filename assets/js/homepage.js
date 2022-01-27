2b597afbe3101428b13e8e4e0fab8c0637b13ef8
Ashley Cho <ashleycho.slp@gmail.com>
2022-01-26 20:12:16 -0800

    language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
     
@@ -18,6 +20,18 @@ var formSubmitHandler = function(event) {
       nameInputEl.value = '';
     } else {
       alert("Please enter a GitHub username");
+    }
+  };
+
+  var buttonClickHandler = function(event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+  
+    if (language) {
+      getFeaturedRepos(language);
+  
+      // clear old content
+      repoContainerEl.textContent = "";
     }
   };
 
@@ -41,6 +55,20 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       // Notice this `.catch()` getting chained onto the end of the `.then()` method
       alert("Unable to connect to GitHub");
+    });
+  };
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
     });
   };
 
@@ -92,3 +120,4 @@ var displayRepos = function(repos, searchTerm) {
   };
   
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
